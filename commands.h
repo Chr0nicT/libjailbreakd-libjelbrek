00bbea92d2412c35747ae82b30c18d4ce5dd4ec0
Chr0nicT <46724628+Chr0nicT@users.noreply.github.com>
2019-05-24 22:43:02 -0400

    command

---
+++ commands.h
@@ -0,0 +1,5 @@
+#define JAILBREAKD_COMMAND_ENTITLE                              1
+#define JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT                  2
+#define JAILBREAKD_COMMAND_ENTITLE_AND_SIGCONT_FROM_XPCPROXY    3
+#define JAILBREAKD_COMMAND_FIXUP_SETUID                         4
+#define JAILBREAKD_COMMAND_UNSANDBOX                            5
